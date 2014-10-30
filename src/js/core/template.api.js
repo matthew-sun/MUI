547e5af72e447f8ef00ceef2f2ac5789e3f1ee11
matthew-sun <527551750@qq.com>
2014-10-30 17:25:41 +0800

    add api

---
+++ src/js/core/template.api.js
@@ -0,0 +1,14 @@
+/**
+ * @file template
+ * @module MUI
+ */
+
+/**
+ * template.js，artTemplate，js模板引擎。
+ * @class template
+ */
+
+/**
+ * @method template
+ * @desc [API文档](https://github.com/aui/artTemplate/wiki/syntax:simple)
+ */
